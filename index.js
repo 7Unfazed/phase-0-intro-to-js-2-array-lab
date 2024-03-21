48c7f2343a83e201c5dd0c9659a56bc698c201dd
Elvis Moses Tangasaiti <elvis.moses@student.moringaschool.com>
2024-03-21 20:50:55 +0300

    creates index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const cats=["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push(name);
+  }
+  function destructivelyPrependCat(name) {
+    cats.unshift(name);
+  }
+  function destructivelyRemoveLastCat() {
+    cats.pop();
+  }
+  function destructivelyRemoveFirstCat() {
+    cats.shift();
+  }
+  function appendCat(){
+  return [...cats,"Broom"];
+  }
+  function prependCat(){
+    return ["Arnold",...cats];
+    }
+    function removeLastCat() {
+        const removeLastCat1 = cats.slice();
+        removeLastCat1.splice(-1, 1);
+        return removeLastCat1;
+    }
+    function   removeFirstCat() {
+      const removeLastCat2 = cats.slice();
+      removeLastCat2.splice(0, 1);
+      return removeLastCat2;
+  }
